7918ce7cd643bceffa6a0d5467b616c268991014
drlagb1703@gmail.com <luis.giraldo3@gmail.com>
2024-05-17 14:56:21 -0500

    first commit

---
+++ logica/Object.py
@@ -0,0 +1,19 @@
+from pygame import Rect
+from pygame import Surface
+class Object:
+    def __init__(self, coordinates:tuple[int]=(0,0), shape:tuple[int] = (0,0), color:tuple[int] = (0, 0)):
+        self.bounds:Rect = Rect(coordinates, shape)
+        self.angle = 0
+        self.color = color
+    
+    def setPosition(self, coordinates:tuple[int]):
+        self.bounds.x, self.bounds.y = coordinates
+
+    def setDrawMethod(self, draw):
+        self.__draw = draw
+
+    def draw(self, matriz:list[bool], surface:Surface):
+        self.__draw()
+
+    def getImage(self):
+        pass
